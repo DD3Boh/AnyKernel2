e3d6b4b924fb7b98c00f9a89551561095e6fcd81
dd3boh <dade.garberi@gmail.com>
2017-05-20 19:15:22 +0200

    DarkMoon: First anykernel2 push
    Signed-off-by: dd3boh <dade.garberi@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,20 +4,20 @@
 ## AnyKernel setup
 # begin properties
 properties() {
-kernel.string=DirtyV by bsmitty83 @ xda-developers
-do.devicecheck=1
-do.modules=0
+kernel.string=
+do.devicecheck=0
+do.initd=0
+do.modules=1
 do.cleanup=1
-do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=
-device.name5=
+device.name1=z2_plus
+device.name2=z2131
+device.name3=Z2
+device.name4=z2
+device.name5=Z2131
 } # end properties
 
 # shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 
 
@@ -29,7 +29,6 @@ is_slot_device=0;
 ## AnyKernel permissions
 # set permissions for included ramdisk files
 chmod -R 755 $ramdisk
-chmod 644 $ramdisk/sbin/media_profiles.xml
 
 
 ## AnyKernel install
@@ -37,32 +36,18 @@ dump_boot;
 
 # begin ramdisk changes
 
-# init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
-append_file init.rc "run-parts" init;
+# begin ramdisk changes
+insert_line default.prop "persist.sys.usb.config=mtp" after "persist.sys.usb.config=none" "ro.patcher.device=z2_plus";
+#nothing changed
 
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "dvbootscript" init.tuna;
+# add custom tuning to init.rc
+insert_line init.qcom.rc "import /init.darkmoon.rc" after "import /init.trace.rc" "import /init.darkmoon.rc\n";
 
-# init.superuser.rc
-if [ -f init.superuser.rc ]; then
-  backup_file init.superuser.rc;
-  replace_string init.superuser.rc "Superuser su_daemon" "# su daemon" "\n# Superuser su_daemon";
-  prepend_file init.superuser.rc "SuperSU daemonsu" init.superuser;
-else
-  replace_file init.superuser.rc 750 init.superuser.rc;
-  insert_line init.rc "init.superuser.rc" after "on post-fs-data" "    import /init.superuser.rc";
-fi;
+# add spectrum support
+insert_line init.qcom.rc "import /init.spectrum.rc" after "import /init.darkmoon.rc" "import /init.spectrum.rc\n";
 
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "nodiratime,barrier=0" "nodev,noatime,nodiratime,barrier=0,data=writeback,noauto_da_alloc,discard";
-patch_fstab fstab.tuna /cache ext4 options "barrier=0,nomblk_io_submit" "nosuid,nodev,noatime,nodiratime,errors=panic,barrier=0,nomblk_io_submit,data=writeback,noauto_da_alloc";
-patch_fstab fstab.tuna /data ext4 options "nomblk_io_submit,data=writeback" "nosuid,nodev,noatime,errors=panic,nomblk_io_submit,data=writeback,noauto_da_alloc";
-append_file fstab.tuna "usbdisk" fstab;
+# remove shit
+remove_line init.qcom.power.rc "setprop sys.io.scheduler"
 
 # end ramdisk changes
 

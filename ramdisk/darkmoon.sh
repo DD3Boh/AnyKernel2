5af8ef3578f2882ee00ae5e9d258132a58688722
dd3boh <dade.garberi@gmail.com>
2017-07-25 16:48:02 +0200

    ramdisk: Remove the zram tweaks

--- ramdisk/darkmoon.sh
+++ ramdisk/darkmoon.sh
@@ -9,18 +9,6 @@ busybox mount -t rootfs -o remount,rw rootfs
 
 BB="/system/xbin/busybox";
 
-#zram tweaks
-echo "0" > /proc/sys/kernel/randomize_va_space;
-echo "0" > /proc/sys/vm/page-cluster;
-echo "10" > /proc/sys/vm/dirty_ratio;
-echo "50" > /proc/sys/vm/vfs_cache_pressure;
-echo "5" > /proc/sys/vm/dirty_background_ratio;
-echo "3000" > /proc/sys/vm/dirty_writeback_centisecs;
-echo "600" > /proc/sys/vm/dirty_expire_centisecs;
-echo "45" > /proc/sys/vm/swappiness;
-echo "4096" > /proc/sys/vm/min_free_kbytes;
-echo "5120000" > /proc/sys/vm/dirty_background_bytes;
-
 # Google Services battery drain fixer by Alcolawl@xda
 pm enable com.google.android.gms/.update.SystemUpdateActivity
 pm enable com.google.android.gms/.update.SystemUpdateService
